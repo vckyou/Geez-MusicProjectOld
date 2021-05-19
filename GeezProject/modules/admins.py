609fc8c1091030370656aa1c873423fbd5c70899
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-19 10:48:45 +0700

    Update admins.py

--- GeezProject/modules/admins.py
+++ GeezProject/modules/admins.py
@@ -1,13 +1,13 @@
 from asyncio.queues import QueueEmpty
-from DaisyXMusic.config import que
+from GeezProject.config import que
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from DaisyXMusic.function.admins import set
-from DaisyXMusic.helpers.channelmusic import get_chat_id
-from DaisyXMusic.helpers.decorators import authorized_users_only, errors
-from DaisyXMusic.helpers.filters import command, other_filters
-from DaisyXMusic.services.callsmusic import callsmusic
+from GeezProject.function.admins import set
+from GeezProject.helpers.channelmusic import get_chat_id
+from GeezProject.helpers.decorators import authorized_users_only, errors
+from GeezProject.helpers.filters import command, other_filters
+from GeezProject.services.callsmusic import callsmusic
 
 
 @Client.on_message(filters.command("adminreset"))
