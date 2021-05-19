5a71b2e2afa12354cd195ad1c9fe7b1f810eadd7
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-19 10:12:20 +0700

    Update __main__.py

--- GeezProject/__main__.py
+++ GeezProject/__main__.py
@@ -1,25 +1,8 @@
-# DaisyXMusic (Telegram bot project)
-# Copyright (C) 2021  Inuka Asith & Rojserbest
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 import requests
 from pyrogram import Client as Bot
 
-from DaisyXMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from DaisyXMusic.services.callsmusic import run
+from GeezProject.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from GeezProject.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
@@ -31,7 +14,7 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="DaisyXMusic.modules"),
+    plugins=dict(root="GeezProject.modules"),
 )
 
 bot.start()
