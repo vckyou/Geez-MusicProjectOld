7112581303c5ca9628e8ac2a2938920216b99dbf
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-19 10:14:11 +0700

    Update config.py

--- GeezProject/config.py
+++ GeezProject/config.py
@@ -1,20 +1,3 @@
-# DAISYXMUSIC- Telegram bot project
-# Copyright (C) 2021  Roj Serbest
-# Copyright (C) 2021  Inuka Asith
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Inukaasith
-
 import os
 from os import getenv
 
@@ -38,7 +21,7 @@ SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DaisySupport_Official")
 PROJECT_NAME = getenv("PROJECT_NAME", "DaisyXMusic v4")
 SOURCE_CODE = getenv("SOURCE_CODE", "github.com/teamdaisyx/daisyxmusic")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
-ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
+ARQ_API_KEY = getenv("ARQ_API_KEY")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
