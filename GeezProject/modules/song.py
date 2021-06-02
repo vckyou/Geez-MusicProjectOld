2f5e80907d7714cf91a8cb1b457a731e5b34c314
Geez <79180058+vckyou@users.noreply.github.com>
2021-06-02 23:16:06 +0700

    Update Fixes Bug

--- GeezProject/modules/song.py
+++ GeezProject/modules/song.py
@@ -17,6 +17,7 @@ from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
+from GeezProject.modules.play import arq
 
 
 
