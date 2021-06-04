3647abb92aa626a77b1238d7c9b095b351fd1c5b
ramadhan73 <81619894+ramadhan73@users.noreply.github.com>
2021-06-04 18:29:50 +0000

    Update play.py

--- GeezProject/modules/play.py
+++ GeezProject/modules/play.py
@@ -467,7 +467,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/ff733cfc3278d6b2d0981.jpg"
+        thumb_name = "https://telegra.ph/file/f6086f8909fbfeb0844f2.png"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
