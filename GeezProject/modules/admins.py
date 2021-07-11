f20ee9b5139294371ebc4a3dd38b834ac77540f0
riz.ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-12 01:13:09 +0700

    Update admins.py

--- GeezProject/modules/admins.py
+++ GeezProject/modules/admins.py
@@ -50,7 +50,7 @@ async def update_admin(client, message: Message):
 @authorized_users_only
 async def pause(_, message: Message):
     callsmusic.pytgcalls.pause_stream(message.chat.id)
-    await message.reply_text("⏸ Music Di Hentikan Sementara.")
+    await message.reply_text("⏸ Music di hentikan sebentar ya..")
 
 
 @Client.on_message(command(["resume", "lanjut"]) & other_filters)
@@ -58,7 +58,7 @@ async def pause(_, message: Message):
 @authorized_users_only
 async def resume(_, message: Message):
     callsmusic.pytgcalls.resume_stream(message.chat.id)
-    await message.reply_text("▶️ Music Dilanjut.")
+    await message.reply_text("▶️ Music dilanjutkan kembali..")
 
 
 @Client.on_message(command(["end", "stop"]) & other_filters)
@@ -71,7 +71,7 @@ async def stop(_, message: Message):
        pass
 
     callsmusic.pytgcalls.leave_group_call(message.chat.id)
-    await message.reply_text("❌ **Menghentikan Lagu!**")
+    await message.reply_text("❌ Lagu dihentikan..")
 
 
 @Client.on_message(command("skip") & other_filters)
@@ -82,7 +82,7 @@ async def skip(_, message: Message):
     chat_id = get_chat_id(message.chat)
 
     callsmusic.queues.task_done(message.chat.id)
-    await message.reply_text("⏩ Melanjut Ke Antrian Selanjutnya.")
+    await message.reply_text("⏩ Melanjut ke antrian selanjutnya..")
     if callsmusic.queues.is_empty(message.chat.id):
         callsmusic.pytgcalls.leave_group_call(message.chat.id)
         await message.reply_text("Music Dihentikan, Antrian Tidak Terdeteksi")
