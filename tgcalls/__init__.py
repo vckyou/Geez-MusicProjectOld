5eee31df829874c4130e22bcdde3847f6e56af82
riz.ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-15 03:55:53 +0700

    Delete __init__.py

--- tgcalls/__init__.py
+++
@@ -1,6 +0,0 @@
-from .__version__ import __version__
-from .methods.logs.py_logs import PyLogs
-from .methods.stream.stream_type import StreamType
-from .pytgcalls import PyTgCalls
-
-__all__ = ('__version__', 'PyLogs', 'StreamType', 'PyTgCalls')
