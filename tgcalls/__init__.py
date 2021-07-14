facb967bcef6f576a8dc930e4092c6c7cb2cd1a2
riz.ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-15 03:40:10 +0700

    Add files via upload

---
+++ tgcalls/__init__.py
@@ -0,0 +1,6 @@
+from .__version__ import __version__
+from .methods.logs.py_logs import PyLogs
+from .methods.stream.stream_type import StreamType
+from .pytgcalls import PyTgCalls
+
+__all__ = ('__version__', 'PyLogs', 'StreamType', 'PyTgCalls')
