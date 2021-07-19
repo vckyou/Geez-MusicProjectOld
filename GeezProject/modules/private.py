6a078e447de81d8280d426f5587f4eafefcf876e
riz.ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-19 20:43:32 +0700

    Update private.py

--- GeezProject/modules/private.py
+++ GeezProject/modules/private.py
@@ -32,9 +32,7 @@ async def start_(client: Client, message: Message):
 
 Saya adalah bot music yang dirancang khusus untuk menemani anda untuk memutar musik di voice chat group dan di channel anda.
 
-Masukkan saya dengan [asisten](https://t.me/rizexxass) saya kedalam grup anda lalu jadikan admin, nikmati musik sepuasnya!
-
-Managed by [rizky](https://t.me/rizexx)""",
+Masukkan saya dengan [asisten](https://t.me/rizexxass) saya kedalam grup anda lalu jadikan admin, nikmati musik sepuasnya!""",
 
 # Edit Yang Seharusnya Lu Edit Aja:D
 # Tapi Jangan di Hapus Special Thanks To nya Yaaa :'D
