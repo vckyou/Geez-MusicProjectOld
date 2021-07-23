df78e2951fabfdbd84ac238ceca65d4a6e97bcbb
vckyou <79180058+vckyou@users.noreply.github.com>
2021-07-24 05:44:02 +0700

    Update __init__.py

--- GeezProject/services/callsmusic/__init__.py
+++ GeezProject/services/callsmusic/__init__.py
@@ -1,2 +1,4 @@
-from GeezProject.services.callsmusic import queues
+from GeezProject.services.queues import queues
 from GeezProject.services.callsmusic.callsmusic import pytgcalls, run
+
+__all__ = ["queues", "pytgcalls", "run"]
