6eb6065beff10f31fdb37d8386550c40081ef5be
vckyou <79180058+vckyou@users.noreply.github.com>
2021-07-24 05:36:06 +0700

    Update ytsearch.py

--- GeezProject/modules/ytsearch.py
+++ GeezProject/modules/ytsearch.py
@@ -36,19 +36,20 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("**/search masukan judul lagu!**")
+            await message.reply_text("/search needs an argument!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎 **Sedang Mencari lagu...**")
+        m = await message.reply_text("Searching....")
         results = YoutubeSearch(query, max_results=4).to_dict()
+        i = 0
         text = ""
-        for i in range(4):
-            text += f"**Judul  :** `{results[i]['title']}`\n"
-            text += f"**Durasi :** {results[i]['duration']}\n"
-            text += f"**Penonton :** {results[i]['views']}\n"
-            text += f"**Channel :** {results[i]['channel']}\n"
+        while i < 4:
+            text += f"Title - {results[i]['title']}\n"
+            text += f"Duration - {results[i]['duration']}\n"
+            text += f"Views - {results[i]['views']}\n"
+            text += f"Channel - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
-            text += "━━\n"
+            i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
         await message.reply_text(str(e))
