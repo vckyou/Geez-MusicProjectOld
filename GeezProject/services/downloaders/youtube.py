08654d3d8c57df7f10e81055a64e3b33079a4504
vckyou <79180058+vckyou@users.noreply.github.com>
2021-07-24 05:40:28 +0700

    Update youtube.py

--- GeezProject/services/downloaders/youtube.py
+++ GeezProject/services/downloaders/youtube.py
@@ -5,13 +5,13 @@ from youtube_dl import YoutubeDL
 from GeezProject.config import DURATION_LIMIT
 from GeezProject.helpers.errors import DurationLimitError
 
-
 ydl_opts = {
-    "format": "bestaudio/best",
+    "format": "bestaudio[ext=m4a]",
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
 }
+
 ydl = YoutubeDL(ydl_opts)
 
 
@@ -21,8 +21,12 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
-
-    ydl.download([url])
+    try:
+        ydl.download([url])
+    except:
+        raise DurationLimitError(
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+        )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
