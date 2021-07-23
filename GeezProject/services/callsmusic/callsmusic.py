f5b1cc01985c619a21c770863a8e3b3f314e97d5
vckyou <79180058+vckyou@users.noreply.github.com>
2021-07-24 05:46:16 +0700

    Update callsmusic.py

--- GeezProject/services/callsmusic/callsmusic.py
+++ GeezProject/services/callsmusic/callsmusic.py
@@ -19,7 +19,7 @@ from pyrogram import Client
 from pytgcalls import PyTgCalls
 
 from GeezProject.config import API_HASH, API_ID, SESSION_NAME
-from GeezProject.services.callsmusic import queues
+from GeezProject.services.queues import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
