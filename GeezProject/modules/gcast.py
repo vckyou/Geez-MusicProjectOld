6d19874aaae29b63462b06ae2971abfb9f6ad3ba
vckyou <79180058+vckyou@users.noreply.github.com>
2021-08-07 09:01:39 +0700

    okeh

--- GeezProject/modules/gcast.py
+++ GeezProject/modules/gcast.py
@@ -17,26 +17,31 @@
 
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
+from pyrogram.types import Dialog, Chat, Message
 import asyncio
 from GeezProject.config import SUDO_USERS
 
 @Client.on_message(filters.command(["gcast"]))
-async def bye(client, message):
+async def broadcast(_, message: Message):
     sent=0
     failed=0
-    if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("Starting Gcast")
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        yaok = await message.reply("`starting broadcast...`")
         if not message.reply_to_message:
-            await lol.edit("Reply to any text message to gcast sir")
+            await yaok.edit("please reply to a message to do broadcast!")
             return
-        msg = message.reply_to_message.text
+        lmao = message.reply_to_message.text
         async for dialog in client.iter_dialogs():
             try:
-                await client.send_message(dialog.chat.id, msg)
+                await client.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await yaok.edit(f"`global cast...` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
+                await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
-            await asyncio.sleep(3)
-        await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
+                #await wtf.edit(f"`broadcasting...` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
+                
+            
+        await message.reply_text(f"`gcast succesfully` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
