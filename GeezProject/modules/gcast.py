0e7820e00d1e189c7857aee0d394617e1ad85aab
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-08 10:56:42 +0700

    fixed gcast

--- GeezProject/modules/gcast.py
+++ GeezProject/modules/gcast.py
@@ -1,24 +1,16 @@
 # Daisyxmusic (Telegram bot project )
 # Copyright (C) 2021  Inukaasith
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# recode by levina-lab on github
+# originally rewritten by using existing code (fixed)
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+import asyncio
 
 from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant
 from pyrogram.types import Dialog, Chat, Message
-import asyncio
+from pyrogram.errors import UserAlreadyParticipant
+
+from GeezProject.services.callsmusic.callsmusic import client as geez
 from GeezProject.config import SUDO_USERS
 
 @Client.on_message(filters.command(["gcast"]))
@@ -28,20 +20,17 @@ async def broadcast(_, message: Message):
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        yaok = await message.reply("`starting broadcast...`")
+        wtf = await message.reply("`memulai global cast...`")
         if not message.reply_to_message:
-            await yaok.edit("please reply to a message to do broadcast!")
+            await wtf.edit("balas ke pesan untuk melakukan broadcast!")
             return
         lmao = message.reply_to_message.text
-        async for dialog in client.iter_dialogs():
+        async for dialog in geez.iter_dialogs():
             try:
-                await client.send_message(dialog.chat.id, lmao)
+                await geez.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await yaok.edit(f"`global cast...` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
+                await wtf.edit(f"`global cast...` \n\n**mengirim ke:** `{sent}` obrolan \n**gagal di:** {failed} obrolan")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-                #await wtf.edit(f"`broadcasting...` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
-                
-            
-        await message.reply_text(f"`gcast succesfully` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
+        await message.reply_text(f"`gcast berhasil` \n\n**terkirim ke:** `{sent}` obrolan \n**gagal di:** {failed} obrolan")
