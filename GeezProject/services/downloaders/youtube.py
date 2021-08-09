38512ced892f0fd80d8deac4157408120e10305b
vckyou <79180058+vckyou@users.noreply.github.com>
2021-08-09 14:24:35 +0700

    Update youtube.py

--- GeezProject/services/downloaders/youtube.py
+++ GeezProject/services/downloaders/youtube.py
@@ -2,8 +2,8 @@ from os import path
 
 from youtube_dl import YoutubeDL
 
-from DaisyXMusic.config import DURATION_LIMIT
-from DaisyXMusic.helpers.errors import DurationLimitError
+from GeezProject.config import DURATION_LIMIT
+from GeezProject.helpers.errors import DurationLimitError
 
 ydl_opts = {
     "format": "bestaudio/best",
