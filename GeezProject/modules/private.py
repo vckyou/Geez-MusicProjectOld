4f2ee3e8f2ede94f8678198fa27bb9fe561c1b04
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-08-16 13:28:14 +0700

    Update private.py

--- GeezProject/modules/private.py
+++ GeezProject/modules/private.py
@@ -59,7 +59,7 @@ Ingin Menambahkan Saya ke Grup Anda? Tambahkan Saya Ke Group Anda!
                         "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
                 ],[
                     InlineKeyboardButton(
-                        "🎁 Donate", url=f"https://t.me/VckyouuBitch")
+                        "🎁 Donate", url=f"https://t.me/fl0werboy")
                 ]
             ]
         ),
@@ -168,10 +168,10 @@ async def reload(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "Group Support", url=f"https://t.me/GeezSupportGroup"
+                        "⚡Group Support⚡", url=f"https://t.me/zzonateman"
                     ),
                     InlineKeyboardButton(
-                        "Created By", url=f"https://t.me/VckyouuBitch"
+                        "🔥Created By🔥", url=f"https://t.me/fl0werboy"
                     )
                 ]
             ]
